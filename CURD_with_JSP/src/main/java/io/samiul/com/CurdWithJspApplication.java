1a1c142ee3db83aee16762822960ea90c4424059
Samiul_Islam <samiulislamw.w.w@gmail.com>
2020-04-30 21:59:33 +0600

    Create spring project

---
+++ CURD_with_JSP/src/main/java/io/samiul/com/CurdWithJspApplication.java
@@ -0,0 +1,13 @@
+package io.samiul.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdWithJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdWithJspApplication.class, args);
+	}
+
+}
---
+++ CURD_with_JSP/src/test/java/io/samiul/com/CurdWithJspApplicationTests.java
@@ -0,0 +1,13 @@
+package io.samiul.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdWithJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
